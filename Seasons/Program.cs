06773503c9040063df7a30939de9d87d90d9997f
wxhami <d18102002@mail.ru>
2022-11-13 19:25:11 +0300

    all done

--- Seasons/Program.cs
+++ Seasons/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Enter date your birthday");
 
-Console.WriteLine("Hello, World!");
+DateTime dateTime = new DateTime();
+
+try
+{
+    dateTime = DateTime.Parse(Console.ReadLine());
+}
+catch (Exception e)
+{
+    Console.WriteLine("incorrect date");
+    return;
+}
+
+ProcessTime(dateTime);
+
+
+
+void ProcessTime(DateTime dateTime)
+{
+   
+    
+    switch (dateTime.Month)
+    {
+        case 1:
+        case 2:
+        case 12:
+            Console.WriteLine("Winter");
+            break;
+
+        case 3:
+        case 4:
+        case 5:
+            Console.WriteLine("spring");
+            break;
+
+        case 6:
+        case 7:
+        case 8:
+            Console.WriteLine("summer");
+            break;
+
+        case 9:
+        case 10:
+        case 11:
+            Console.WriteLine("autumn");
+            break;
+    }
+}
